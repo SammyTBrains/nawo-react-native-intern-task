275ad6b373c5fc9966917178f149367eba43254c
Samuel Joseph <josephsamuel034@gmail.com>
2024-06-01 18:26:01 +0100

    bgd seperate

--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -6,8 +6,7 @@ import { Ionicons } from "@expo/vector-icons";
 import HomeHeader from "../components/Home/HomeHeader";
 import UserInfoSummary from "../components/Home/UserInfoSummary";
 import TransactionSortLink from "../components/Home/TransactionSortLink";
-import ArrowButton from "../components/UI/ArrowButton";
-import { ProgressBarSVG } from "../components/svgs";
+import BudgetsSection from "../components/Home/BudgetsSection";
 
 const Home = () => {
   return (
@@ -16,29 +15,7 @@ const Home = () => {
         <HomeHeader />
         <UserInfoSummary />
         <TransactionSortLink />
-        <View style={styles.budgetsSection}>
-          <Text style={styles.budgetsHeaderLabel}>My Budgets</Text>
-          <View style={styles.budgetsCard}>
-            <View style={styles.budgetsCardTopSection}>
-              <View style={styles.budgetCardsTopRow}>
-                <Text style={styles.budgetsCardText}>You have</Text>
-                <ArrowButton
-                  style={{ height: 24, width: 24, borderRadius: 24 }}
-                />
-              </View>
-              <Text style={styles.budgetsCardTopSectionPrice}>N29,880</Text>
-              <Text style={styles.budgetsCardText}>
-                Left out of N80,888 budgeted
-              </Text>
-            </View>
-            <View style={{ marginVertical: 32 }}>
-              <ProgressBarSVG />
-            </View>
-            <Text style={styles.budgetsCardText}>
-              😱 Sapa go soon catch you bros, calm down!!
-            </Text>
-          </View>
-        </View>
+        <BudgetsSection />
       </ScrollView>
     </SafeAreaView>
   );
@@ -51,31 +28,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: GlobalStyles.colors.primary,
     padding: 16,
-  },
-  budgetsSection: {
-    marginTop: 32,
-  },
-  budgetsHeaderLabel: { color: "#C1B9F9", fontSize: 13, fontWeight: "bold" },
-  budgetsCard: {
-    padding: 24,
-    marginTop: 16,
-    backgroundColor: GlobalStyles.colors.primaryLight,
-    borderRadius: 24,
-  },
-  budgetsCardTopSection: { gap: 16 },
-  budgetsCardText: {
-    color: "white",
-    fontSize: 12,
-    fontWeight: "medium",
-  },
-  budgetsCardTopSectionPrice: {
-    color: "white",
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  budgetCardsTopRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
   },
 });
---
+++ components/Home/BudgetsSection.tsx
@@ -0,0 +1,62 @@
+import { Text, StyleSheet, View } from "react-native";
+
+import { GlobalStyles } from "../../constants/styles";
+import ArrowButton from "../../components/UI/ArrowButton";
+import { ProgressBarSVG } from "../../components/svgs";
+
+const BudgetsSection = () => {
+  return (
+    <View style={styles.budgetsSection}>
+      <Text style={styles.budgetsHeaderLabel}>My Budgets</Text>
+      <View style={styles.budgetsCard}>
+        <View style={styles.budgetsCardTopSection}>
+          <View style={styles.budgetCardsTopRow}>
+            <Text style={styles.budgetsCardText}>You have</Text>
+            <ArrowButton style={{ height: 24, width: 24, borderRadius: 24 }} />
+          </View>
+          <Text style={styles.budgetsCardTopSectionPrice}>N29,880</Text>
+          <Text style={styles.budgetsCardText}>
+            Left out of N80,888 budgeted
+          </Text>
+        </View>
+        <View style={{ marginVertical: 32 }}>
+          <ProgressBarSVG />
+        </View>
+        <Text style={styles.budgetsCardText}>
+          😱 Sapa go soon catch you bros, calm down!!
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default BudgetsSection;
+
+const styles = StyleSheet.create({
+  budgetsSection: {
+    marginTop: 32,
+  },
+  budgetsHeaderLabel: { color: "#C1B9F9", fontSize: 13, fontWeight: "bold" },
+  budgetsCard: {
+    padding: 24,
+    marginTop: 16,
+    backgroundColor: GlobalStyles.colors.primaryLight,
+    borderRadius: 24,
+  },
+  budgetsCardTopSection: { gap: 16 },
+  budgetsCardText: {
+    color: "white",
+    fontSize: 12,
+    fontWeight: "medium",
+  },
+  budgetsCardTopSectionPrice: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  budgetCardsTopRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
