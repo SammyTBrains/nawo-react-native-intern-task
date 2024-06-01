29ed64a16b98ff6d082aa39e4e327738c37d73e4
Samuel Joseph <josephsamuel034@gmail.com>
2024-06-01 15:56:22 +0100

    top summ

--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -17,6 +17,28 @@ const Home = () => {
               style={{ width: 52, height: 52 }}
             />
           </View>
+          <View style={{ marginVertical: 16, gap: 16 }}>
+            <View style={{ gap: 8 }}>
+              <Text
+                style={{ textAlign: "center", color: "white", fontSize: 11 }}
+              >
+                Your available balance is
+              </Text>
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 28,
+                  fontWeight: "bold",
+                  color: "white",
+                }}
+              >
+                N20,983
+              </Text>
+            </View>
+            <Text style={{ textAlign: "center", color: "white", fontSize: 11 }}>
+              By this time last month, you spent slightly higher (N22,719)
+            </Text>
+          </View>
         </View>
       </View>
     </SafeAreaView>
@@ -32,13 +54,15 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   userSummary: {
-    padding: 10,
+    padding: 24,
     marginTop: 16,
     backgroundColor: "#210FA4",
     borderRadius: 24,
+    alignItems: "center",
   },
   userSummaryTopDetails: {
     alignItems: "center",
+    width: 192,
   },
   userImageBG: {
     backgroundColor: "#BDD6FF",
@@ -48,4 +72,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 52,
   },
+  userSummaryT: {},
 });
