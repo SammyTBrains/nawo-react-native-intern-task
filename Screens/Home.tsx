95cd47b95865720003c418965b2e270ddec31891
Samuel Joseph <josephsamuel034@gmail.com>
2024-06-01 15:41:59 +0100

    h

--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -1,4 +1,4 @@
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Image } from "react-native";
 import { GlobalStyles } from "../constants/styles";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
@@ -9,6 +9,16 @@ const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
       <HomeHeader />
+      <View style={styles.userSummary}>
+        <View style={styles.userSummaryTopDetails}>
+          <View style={styles.userImageBG}>
+            <Image
+              source={require("../assets/images/userDummyImage.png")}
+              style={{ width: 52, height: 52 }}
+            />
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -20,5 +30,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: GlobalStyles.colors.primary,
     padding: 16,
+  },
+  userSummary: {
+    padding: 10,
+    marginTop: 16,
+    backgroundColor: "#210FA4",
+    borderRadius: 24,
+  },
+  userSummaryTopDetails: {
+    alignItems: "center",
+  },
+  userImageBG: {
+    backgroundColor: "#BDD6FF",
+    width: 52,
+    height: 52,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 52,
   },
 });
